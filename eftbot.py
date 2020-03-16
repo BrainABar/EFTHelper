e447f6ed4fc23c3c99d95af2495272f35ae1509a
bryan barajas <bryanbarajas1996@gmail.com>
2020-03-16 02:05:43 -0700

    aliases added with ping test function

--- eftbot.py
+++ eftbot.py
@@ -4,8 +4,19 @@ from config import Config
 
 client = commands.Bot(command_prefix='!')
 
+
 @client.event
 async def on_ready():
     print("On ready")
+
+
+@client.event
+async def on_member_join(member):
+    print(f'(member) joined')
+
+
+@client.command(aliases=['ping', 'pings'])
+async def _ping(ctx):
+    await ctx.send(f"Ping {round(client.latency *1000)}ms")
 
 client.run(Config.BOT_TOKEN)
