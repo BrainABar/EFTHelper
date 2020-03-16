e27e215278964baa4c25e093691e8d02cee3f6c8
bryan barajas <bryanbarajas1996@gmail.com>
2020-03-16 03:11:01 -0700

    error event handling

--- eftbot.py
+++ eftbot.py
@@ -7,6 +7,8 @@ from config import Config
 client = commands.Bot(command_prefix="!")
 status = cycle(['test status 1', 'status test 2'])
 
+
+# Events
 @client.event
 async def on_ready():
     set_status.start()
@@ -14,20 +16,35 @@ async def on_ready():
 
 
 @client.event
+async def command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Command does not exist")
+
+
+@client.event
 async def on_member_join(member):
     print(f'(member) joined')
 
 
+
+# Commands
 @client.command(aliases=['ping', 'pings'])
 async def _ping(ctx):
     await ctx.send(f"Ping {round(client.latency *1000)}ms")
 
 
 @client.command()
-async def clear(ctx, amount=5):
+async def clear(ctx, amount=):
     await ctx.channel.purge(limit=amount+1)
 
 
+@clear.error
+async def clear_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Send in all command requirements")
+
+
+# Update function @ intervals
 @tasks.loop(minutes=30)
 async def set_status():
     await client.change_presence(activity=discord.Game(next(status)))
